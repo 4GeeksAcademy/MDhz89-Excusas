417831c4a83e307fe7e0bf52ea5e96d93eeb82b9
MDhz89 <mdjho89@gmail.com>
2024-07-30 02:37:19 +0000

    Guardar archivo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let rwh = Math.floor(Math.random() * who.length);
+  let ra = Math.floor(Math.random() * action.length);
+  let rwa = Math.floor(Math.random() * what.length);
+  let rwhe = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excusa").innerHTML =
+    who[rwh] + " " + action[ra] + " " + what[rwa] + " " + when[rwhe];
 };
